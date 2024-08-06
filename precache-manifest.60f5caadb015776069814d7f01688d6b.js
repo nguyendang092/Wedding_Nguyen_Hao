6fda121facc12a84970f3ed99d0aabfcb65ac24b
nguyendang092 <trungnguyencv092@gmail.com>
2024-08-06 15:56:23 +0700

    Updates

---
+++ precache-manifest.60f5caadb015776069814d7f01688d6b.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bacc5eabcf38598d3d39d1f680a7b18",
+    "url": "/Wedding_Nguyen_Hao/index.html"
+  },
+  {
+    "revision": "19c95bf00d74bf148efc",
+    "url": "/Wedding_Nguyen_Hao/static/css/2.2bce5cab.chunk.css"
+  },
+  {
+    "revision": "e53b7902b40128861d21",
+    "url": "/Wedding_Nguyen_Hao/static/css/main.fef9b2ea.chunk.css"
+  },
+  {
+    "revision": "19c95bf00d74bf148efc",
+    "url": "/Wedding_Nguyen_Hao/static/js/2.60041837.chunk.js"
+  },
+  {
+    "revision": "e53b7902b40128861d21",
+    "url": "/Wedding_Nguyen_Hao/static/js/main.d49f389c.chunk.js"
+  },
+  {
+    "revision": "c8cfc2233fefe1dd8be8",
+    "url": "/Wedding_Nguyen_Hao/static/js/runtime~main.89f35fcb.js"
+  },
+  {
+    "revision": "9008302282fc8e1e7c7fd196b61d104b",
+    "url": "/Wedding_Nguyen_Hao/static/media/1.90083022.jpg"
+  },
+  {
+    "revision": "0e09ad1a577203ee967ff3ba12e60915",
+    "url": "/Wedding_Nguyen_Hao/static/media/BichTram_600x600.0e09ad1a.jpg"
+  },
+  {
+    "revision": "4abb8af4047ebd9c683b37aa75e7789c",
+    "url": "/Wedding_Nguyen_Hao/static/media/ChangDuongXaXoi.4abb8af4.png"
+  },
+  {
+    "revision": "15cf01b77e18000ff8c30e5eb8132326",
+    "url": "/Wedding_Nguyen_Hao/static/media/Flaticon.15cf01b7.woff"
+  },
+  {
+    "revision": "5f762089c6bc050930a5ae01ef95e746",
+    "url": "/Wedding_Nguyen_Hao/static/media/Flaticon.5f762089.svg"
+  },
+  {
+    "revision": "7ad910ab912a8296b41d09126628d1a4",
+    "url": "/Wedding_Nguyen_Hao/static/media/Flaticon.7ad910ab.ttf"
+  },
+  {
+    "revision": "b9b714e8ebdc31809b71111250ee257e",
+    "url": "/Wedding_Nguyen_Hao/static/media/Flaticon.b9b714e8.eot"
+  },
+  {
+    "revision": "a5e6c3e45c630cde83bdcb987ce8aca2",
+    "url": "/Wedding_Nguyen_Hao/static/media/LanDauGapGo.a5e6c3e4.png"
+  },
+  {
+    "revision": "24647132916847e724178f2387330aca",
+    "url": "/Wedding_Nguyen_Hao/static/media/LeCuoi.24647132.png"
+  },
+  {
+    "revision": "12281f6f581dd8ec68ca59eda5c4ccbb",
+    "url": "/Wedding_Nguyen_Hao/static/media/LeThanhHuy.12281f6f.jpg"
+  },
+  {
+    "revision": "fa5c9b04cd8258f1fb24a988566ac9b7",
+    "url": "/Wedding_Nguyen_Hao/static/media/NgocLinh_600x600.fa5c9b04.jpg"
+  },
+  {
+    "revision": "5d4c35786265c4598540495d80e1c002",
+    "url": "/Wedding_Nguyen_Hao/static/media/NguyenChiPhuong.5d4c3578.jpg"
+  },
+  {
+    "revision": "ac3c94ceaad6dfe908ac78aa7494a2a2",
+    "url": "/Wedding_Nguyen_Hao/static/media/NguyenNhuSam.ac3c94ce.jpg"
+  },
+  {
+    "revision": "e96b1844c86b9ffc2ec9014f3736f5da",
+    "url": "/Wedding_Nguyen_Hao/static/media/NhuY_600x600.e96b1844.jpg"
+  },
+  {
+    "revision": "30480f4e70eb3e72e7e65099fd28d6b3",
+    "url": "/Wedding_Nguyen_Hao/static/media/NhungLanHonDoi.30480f4e.png"
+  },
+  {
+    "revision": "9356fc2da94e0b9f3e10e39d76391862",
+    "url": "/Wedding_Nguyen_Hao/static/media/PhamHuuPhuoc.9356fc2d.jpg"
+  },
+  {
+    "revision": "d515f3d97768aef1be42d47c46b5d438",
+    "url": "/Wedding_Nguyen_Hao/static/media/TiecCuoi1.d515f3d9.png"
+  },
+  {
+    "revision": "0f1524a24fffcb236623f4052ca35aa3",
+    "url": "/Wedding_Nguyen_Hao/static/media/TiecCuoi2.0f1524a2.png"
+  },
+  {
+    "revision": "b2e83c53d020117028ff4b89b1202c55",
+    "url": "/Wedding_Nguyen_Hao/static/media/TraiTim.b2e83c53.png"
+  },
+  {
+    "revision": "37cdf6bfb7aeb3375dcc1fa2e5bbd176",
+    "url": "/Wedding_Nguyen_Hao/static/media/Tram_600x600.37cdf6bf.jpg"
+  },
+  {
+    "revision": "b42040d878495eba257099d602c1aed0",
+    "url": "/Wedding_Nguyen_Hao/static/media/Trang_600x600.b42040d8.jpg"
+  },
+  {
+    "revision": "8aeadb9494076d29d9ae7f14fe5fcd42",
+    "url": "/Wedding_Nguyen_Hao/static/media/YenNhi_600x600.8aeadb94.jpg"
+  },
+  {
+    "revision": "f9a5ca88df21a021cb01b47ca3f22dba",
+    "url": "/Wedding_Nguyen_Hao/static/media/backgroundWelcomeArea.f9a5ca88.png"
+  },
+  {
+    "revision": "9ca6cddd771b480c435ac3efe4376354",
+    "url": "/Wedding_Nguyen_Hao/static/media/banking_MB_200x150.9ca6cddd.png"
+  },
+  {
+    "revision": "1e420dbf026041ccd1e9fe724430942f",
+    "url": "/Wedding_Nguyen_Hao/static/media/banking_VCB_200x150.1e420dbf.png"
+  },
+  {
+    "revision": "3831b6b6f5b63fdb7f88001514f2c4c6",
+    "url": "/Wedding_Nguyen_Hao/static/media/bgrWedding1.3831b6b6.png"
+  },
+  {
+    "revision": "28fef16115f15a04f5018764b97ef85c",
+    "url": "/Wedding_Nguyen_Hao/static/media/bgrWedding2.28fef161.jpg"
+  },
+  {
+    "revision": "123b7d37cd8d85017b593ad45c328a13",
+    "url": "/Wedding_Nguyen_Hao/static/media/close.123b7d37.svg"
+  },
+  {
+    "revision": "b3f6eb9109e76c2e109905fe96cc7b65",
+    "url": "/Wedding_Nguyen_Hao/static/media/countdown_400x140.b3f6eb91.png"
+  },
+  {
+    "revision": "0fa3c4c452a8b03087028fa38a54db34",
+    "url": "/Wedding_Nguyen_Hao/static/media/couple_1_500x500.0fa3c4c4.jpg"
+  },
+  {
+    "revision": "4a7cbbea4c9d9a619f6282444c7b0b05",
+    "url": "/Wedding_Nguyen_Hao/static/media/couple_2_500x500.4a7cbbea.jpg"
+  },
+  {
+    "revision": "cef8abfdbb558ad2ef458a21dfe86714",
+    "url": "/Wedding_Nguyen_Hao/static/media/footer2.cef8abfd.svg"
+  },
+  {
+    "revision": "3081663da73eaf367658e34903af5c28",
+    "url": "/Wedding_Nguyen_Hao/static/media/gal_1_600x746.3081663d.jpg"
+  },
+  {
+    "revision": "b1abf0e54ce68ba4e0d370454438448d",
+    "url": "/Wedding_Nguyen_Hao/static/media/gal_2_600x746.b1abf0e5.jpg"
+  },
+  {
+    "revision": "244a20710242bfea85526392dfc377f5",
+    "url": "/Wedding_Nguyen_Hao/static/media/gal_3_600x746.244a2071.jpg"
+  },
+  {
+    "revision": "9150582ede1813a6878335410addd6bf",
+    "url": "/Wedding_Nguyen_Hao/static/media/gal_4_600x746.9150582e.jpg"
+  },
+  {
+    "revision": "5aa3b55cbd6bbb639cd380baf3506802",
+    "url": "/Wedding_Nguyen_Hao/static/media/gal_5_600x746.5aa3b55c.jpg"
+  },
+  {
+    "revision": "ea6074b5c8ca270503b76e14fd063484",
+    "url": "/Wedding_Nguyen_Hao/static/media/gal_6_600x746.ea6074b5.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Wedding_Nguyen_Hao/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Wedding_Nguyen_Hao/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Wedding_Nguyen_Hao/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Wedding_Nguyen_Hao/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "5060bbf0671aa50ec4824dbb9f33eab0",
+    "url": "/Wedding_Nguyen_Hao/static/media/wait_400x140.5060bbf0.jpg"
+  }
+]);
